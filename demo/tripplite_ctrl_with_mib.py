ca0cc14aaded34b55b8f957a4945d49cec4f8bf0
Yipeng Sun <syp@umd.edu>
2018-04-02 17:34:49 -0400

    Fix: automatically generate the mib path.

--- demo/tripplite_ctrl_with_mib.py
+++ demo/tripplite_ctrl_with_mib.py
@@ -1,30 +1,35 @@
 #!/usr/bin/env python
 #
-# Last Change: Mon Apr 02, 2018 at 03:53 PM -0400
+# Last Change: Mon Apr 02, 2018 at 05:34 PM -0400
 
 import sys
-import os.path as path
+from os.path import dirname, abspath, join
 
 from pysnmp.hlapi import *
 from pysnmp.smi import builder, view, compiler
+
+# The absolute path of the mib files
+mib_path = 'file://' + join(
+    dirname(dirname(abspath(__file__))), 'MIB', 'Tripp_Lite')
 
 # Compile mib
 mibBuilder = builder.MibBuilder()
 mibViewController = view.MibViewController(mibBuilder)
 compiler.addMibCompiler(mibBuilder, sources=[
-    'file:///home/syp/misc/researches/umd-jawahery/labSNMP/MIB',
+    mib_path,
     'http://mibs.snmplabs.com/asn1/@mib@'])
 
 # Load mib
-mibBuilder.loadModules('TRIPPLITE-PRODUCTS', 'TRIPPLITE', 'SNMPv2-MIB')
+mibBuilder.loadModules('TRIPPLITE-PRODUCTS', 'TRIPPLITE')
 
 # Get the name of the SMTP command that will be executed
-cmd = ObjectIdentity('TRIPPLITE-PRODUCTS', 'tlpHardware', 0)
+cmd = ObjectIdentity('TRIPPLITE-PRODUCTS', sys.argv[2], 0)
 
 # Perform lookup
 g = getCmd(SnmpEngine(),
            CommunityData('public'),
-           UdpTransportTarget((sys.argv[1], 161)),
+           UdpTransportTarget((sys.argv[2], 161)),
+           ContextData(),
            ObjectType(cmd))
 next(g)
 
