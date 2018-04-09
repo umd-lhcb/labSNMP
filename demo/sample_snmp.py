2b023781d83789b947e292e488cc64af09718088
Yipeng Sun <syp@umd.edu>
2018-04-09 01:28:04 -0400

    A minimal example on how to perform and parse snmp inquiry.

--- demo/sample_snmp.py
+++ demo/sample_snmp.py
@@ -3,13 +3,32 @@
 import sys
 from pysnmp.hlapi import *
 
-x = ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0)
-g = getCmd(SnmpEngine(),
-           CommunityData('tripplite'),
-           UdpTransportTarget((sys.argv[1], 161)),
-           ContextData(),
-           ObjectType(x))
+# The 'Test' parameter shows how to pass a argument to the command.
+oidtype = ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0), 'Test')
 
-result = next(g)
-print(result)
-print(str(x))
+# if we replace 'getCmd' with 'nextCmd', we will loop through all commands
+# presented in the 'SNMPv2' mib file.
+querycmd = getCmd(SnmpEngine(),
+                  CommunityData('public'),
+                  UdpTransportTarget((sys.argv[1], 161)),
+                  ContextData(),
+                  oidtype)
+
+for (errorIndication,
+     errorStatus,
+     errorIndex,
+     varBinds) in querycmd:
+
+    if errorIndication:
+        print(errorIndication)
+        break
+
+    elif errorStatus:
+        print('%s at %s' % (errorStatus.prettyPrint(),
+                            errorIndex and varBinds[int(errorIndex) - 1][0] or
+                            '?'))
+        break
+
+    else:
+        for varBind in varBinds:
+            print(' = '.join([x.prettyPrint() for x in varBind]))
