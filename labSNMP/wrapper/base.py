4a2576e35d639f647c613a9e0d53a406578c57af
Yipeng Sun <syp@umd.edu>
2018-04-12 17:24:05 -0400

    Define the methods that a general power control should have.

--- labSNMP/wrapper/base.py
+++ labSNMP/wrapper/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Last Change: Thu Apr 12, 2018 at 05:20 PM -0400
+# Last Change: Thu Apr 12, 2018 at 05:23 PM -0400
 
 from os import environ
 from os.path import dirname, abspath, join
@@ -12,6 +12,12 @@ py_mib_path = join(dirname(dirname(abspath(__file__))), 'compiled')
 
 class BasePowerSupplyControl(object):
     community = 'public'
+    total_chs = 0
+    power_status_code = {
+        'on': 0,
+        'off': 1,
+        'cycle': 2
+    }
 
     def __init__(self, ip):
         # FIXME: Use environmental variable to enable PySNMP to load compiled
@@ -29,3 +35,24 @@ class BasePowerSupplyControl(object):
             ContextData(),
             oidtype
         )
+
+    def PowerOffCh(self, ch_num):
+        pass
+
+    def PowerOnCh(self, ch_num):
+        pass
+
+    def PowerCycleCh(self, ch_num):
+        pass
+
+    def PowerOffAll(self):
+        pass
+
+    def PowerOnAll(self):
+        pass
+
+    def PowerCycleAll(self):
+        pass
+
+    def ChStatus(self, ch_num):
+        pass
