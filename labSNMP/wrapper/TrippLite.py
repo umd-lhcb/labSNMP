338f77eb1cd6f24630ab9b6cf6cfd2273a12d35c
Yipeng Sun <syp@umd.edu>
2018-04-13 12:10:13 -0400

    Working on the controlling of the TrippLite unit.

---
+++ labSNMP/wrapper/TrippLite.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#
+# Last Change: Fri Apr 13, 2018 at 12:09 PM -0400
+
+from pysnmp.hlapi import *
+
+from labSNMP.wrapper.base import BiDict, BasePowerSupplyControl
+
+
+class TrippLiteControl(BasePowerSupplyControl):
+    community = 'tripplite'
+    total_chs = 14
+    power_status_code = BiDict({
+        'idle':  0,
+        'off':   1,
+        'on':    2,
+        'cycle': 3
+    })
+
+    MIB = 'TRIPPLITE-PRODUCTS'
+    ch_ctrl = 'tlpPduOutletComand'
+    bulk_ctrl = 'tlpPduDeviceMainLoadCommand'
+    ch_status = 'tlpPduOutletState'
+
+    def PowerOffCh(self, ch_num):
+        oidPowerOff = OjectType(ObjectIdentity(self.MIB, self.ch_ctrl),
+                                self.power_status_code['off']
+                                )
+        status = self.DoCmd(setCmd, oidPowerOff)
+
+        print(status)
--- labSNMP/wrapper/base.py
+++ labSNMP/wrapper/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Last Change: Thu Apr 12, 2018 at 05:23 PM -0400
+# Last Change: Fri Apr 13, 2018 at 12:08 PM -0400
 
 from os import environ
 from os.path import dirname, abspath, join
@@ -10,14 +10,23 @@ from pysnmp.smi import builder
 py_mib_path = join(dirname(dirname(abspath(__file__))), 'compiled')
 
 
+class BiDict(dict):
+    # NOTE: Here we implicitly assumed that the forward mapping is injective.
+    def __init__(self, *args, **kwargs):
+        super(BiDict, self).__init__(*args, **kwargs)
+        self.inverse = {}
+        for key, value in self.iteritems():
+            self.inverse[value] = key
+
+
 class BasePowerSupplyControl(object):
     community = 'public'
     total_chs = 0
-    power_status_code = {
-        'on': 0,
-        'off': 1,
+    power_status_code = BiDict({
+        'on':    0,
+        'off':   1,
         'cycle': 2
-    }
+    })
 
     def __init__(self, ip):
         # FIXME: Use environmental variable to enable PySNMP to load compiled
@@ -28,13 +37,35 @@ class BasePowerSupplyControl(object):
         self.mibBuilder = builder.MibBuilder()
 
     def DoCmd(self, cmd, oidtype):
-        cmd(
+        queryCmd = cmd(
             SnmpEngine(),
             CommunityData(self.community),
             UdpTransportTarget((self.ip, 161)),
             ContextData(),
             oidtype
         )
+
+        try:
+            for (errorIndication,
+                 errorStatus,
+                 errorIndex,
+                 varBinds) in queryCmd:
+
+                if errorIndication:
+                    return errorIndication
+
+                elif errorStatus:
+                    return errorStatus
+
+                else:
+                    status = [0, ]
+                    for varBind in varBinds:
+                        for x in varBind:
+                            status.append(x.prettyPrint())
+                    return status
+
+        except Exception as err:
+            return err
 
     def PowerOffCh(self, ch_num):
         pass
@@ -55,4 +86,7 @@ class BasePowerSupplyControl(object):
         pass
 
     def ChStatus(self, ch_num):
+        pass
+
+    def ChsAllStatus(self):
         pass
