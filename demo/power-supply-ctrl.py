d5c9553b35835edf06cbd04d15f4b1f486930c20
Yipeng Sun <syp@umd.edu>
2018-04-17 00:28:03 -0400

    A fully working control script for TrippLite.

--- demo/power-supply-ctrl.py
+++ demo/power-supply-ctrl.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Last Change: Mon Apr 16, 2018 at 09:20 PM -0400
+# Last Change: Tue Apr 17, 2018 at 12:27 AM -0400
 
 from argparse import ArgumentParser
 
@@ -12,7 +12,7 @@ from labSNMP.wrapper.TrippLite import TrippLiteControl
 
 def parse_input():
     parser = ArgumentParser(
-        description='Control the power states of the PSUs in our lab.',
+        description='Control the power states of the PDUs in our lab.',
     )
 
     required = parser.add_argument_group('required arguments')
@@ -44,8 +44,65 @@ def parse_input():
     return parser.parse_args()
 
 
+class InputToPduAction(object):
+    def __init__(self, controller, input):
+        self.controller = controller
+        self.input = input
+
+    def do(self):
+        try:
+            assert self.check_input_validity()
+            if len(self.input) == 3:
+                if self.input[2] == 'off':
+                    print(self.controller.PowerOffCh(self.input[1]))
+                if self.input[2] == 'on':
+                    print(self.controller.PowerOnCh(self.input[1]))
+                if self.input[2] == 'cycle':
+                    print(self.controller.PowerCycleCh(self.input[1]))
+                if self.input[2] == 'status':
+                    print(self.controller.ChStatus(self.input[1]))
+            else:
+                if self.input[0] == 'off':
+                    print(self.controller.PowerOffAll())
+                if self.input[0] == 'on':
+                    print(self.controller.PowerOnAll())
+                if self.input[2] == 'cycle':
+                    print(self.controller.PowerCycleAll())
+                if self.input[2] == 'status':
+                    print(self.controller.ChsAllStatus())
+
+        except AssertionError:
+            print('Invalid input: %s' % self.input)
+        except Exception as err:
+            print(err)
+
+    def check_input_validity(self):
+        if len(self.input) == 1 or len(self.input) == 3:
+            boolean_list = map(self.valid_keywords, self.input)
+            if True in boolean_list:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    @staticmethod
+    def valid_keywords(word):
+        if word in ('off', 'on', 'cycle', 'status'):
+            return True
+        else:
+            return False
+
+
 if __name__ == "__main__":
     args = parse_input()
 
     if args.mode == 'tripplite':
         controller = TrippLiteControl(args.host[0])
+        worker = InputToPduAction(controller, args.action)
+        worker.do()
+    elif args.mode == 'wiener':
+        pass
+        # controller = TrippLiteControl(args.host[0])
+        # worker = InputToPduAction(controller, args.action)
+        # worker.do()
--- labSNMP/wrapper/TrippLite.py
+++ labSNMP/wrapper/TrippLite.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Last Change: Mon Apr 16, 2018 at 11:56 PM -0400
+# Last Change: Tue Apr 17, 2018 at 12:16 AM -0400
 
 from pysnmp.hlapi import *
 
@@ -23,11 +23,61 @@ class TrippLiteControl(BasePowerSupplyControl):
     ch_status = 'tlpPduOutletState'
 
     def PowerOffCh(self, ch_num):
-        oidPowerOff = ObjectType(ObjectIdentity(
+        oid = ObjectType(ObjectIdentity(
             self.MIB,
             self.ch_ctrl, '1', str(ch_num)
         ),
             self.power_status_code['off']
         )
 
-        return self.DoCmd(setCmd, oidPowerOff)
+        return self.DoCmd(setCmd, oid)
+
+    def PowerOnCh(self, ch_num):
+        oid = ObjectType(ObjectIdentity(
+            self.MIB,
+            self.ch_ctrl, '1', str(ch_num)
+        ),
+            self.power_status_code['on']
+        )
+
+        return self.DoCmd(setCmd, oid)
+
+    def PowerCycleCh(self, ch_num):
+        oid = ObjectType(ObjectIdentity(
+            self.MIB,
+            self.ch_ctrl, '1', str(ch_num)
+        ),
+            self.power_status_code['cycle']
+        )
+
+        return self.DoCmd(setCmd, oid)
+
+    def PowerOffAll(self):
+        oid = ObjectType(ObjectIdentity(
+            self.MIB,
+            self.bulk_ctrl, '0'
+        ),
+            self.power_status_code['off']
+        )
+
+        return self.DoCmd(setCmd, oid)
+
+    def PowerOnAll(self):
+        oid = ObjectType(ObjectIdentity(
+            self.MIB,
+            self.bulk_ctrl, '0'
+        ),
+            self.power_status_code['on']
+        )
+
+        return self.DoCmd(setCmd, oid)
+
+    def PowerCycleAll(self):
+        oid = ObjectType(ObjectIdentity(
+            self.MIB,
+            self.bulk_ctrl, '0'
+        ),
+            self.power_status_code['cycle']
+        )
+
+        return self.DoCmd(setCmd, oid)
