e130c5d792b452e2b8e297afffc9a1781606a77b
Yipeng Sun <syp@umd.edu>
2020-06-28 21:21:55 +0800

    Install Python dev tools for x86_64 platform.

--- shell.nix
+++ shell.nix
@@ -1,12 +1,21 @@
 let
-  pkgs = import <nixpkgs>;
+  pkgs = import <nixpkgs> {};
   python = pkgs.python3;
   pythonPackages = python.pkgs;
+  stdenv = pkgs.stdenv;
 in
 
 pkgs.mkShell {
   name = "labSNMP";
   buildInputs = with pythonPackages; [
     pysnmp
+  ]
+  ++ stdenv.lib.optionals (stdenv.isx86_64) [
+    # Python auto-complete
+    jedi
+
+    # Linters
+    flake8
+    pylint
   ];
 }
